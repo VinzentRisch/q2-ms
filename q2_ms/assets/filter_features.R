b93e89e54cfff603e7f257edbe7c0178460d9f3c
VinzentRisch <risch.vinzent@gmail.com>
2025-02-06 11:28:16 +0100

    bugs

--- q2_ms/assets/filter_features.R
+++ q2_ms/assets/filter_features.R
@@ -18,7 +18,7 @@ option_list <- list(
   make_option(opt_str = "--sample_metadata_column", type = "character"),
   make_option(opt_str = "--na_rm", type = "logical"),
   make_option(opt_str = "--mad", type = "logical"),
-  make_option(opt_str = "--output_path", type = "character"),
+  make_option(opt_str = "--output_path", type = "character")
 )
 
 # Parse arguments
@@ -29,33 +29,31 @@ XCMSExperiment <- readMsObject(XcmsExperiment(), PlainTextParam(opt$xcms_experim
 
 if (opt$xcms_experiment == "rsd") {
     filter <- RsdFilter(
-      threshold = opt$threshold,
-      qcIndex = sampleData(XCMSExperiment)$opt[[sample_metadata_column]] == opt$qc_label,
+      qcIndex = sampleData(XCMSExperiment)[[opt$sample_metadata_column]] == opt$qc_label,
       na.rm = opt$na_rm,
-      mad = FALSE
+      mad = opt$mad
     )
 } else if (opt$xcms_experiment == "d_ratio") {
     filter <- DratioFilter(
-      threshold = opt$threshold,
-      qcIndex = sampleData(XCMSExperiment)$opt[[sample_metadata_column]] == opt$qc_label,
-      studyIndex = sampleData(XCMSExperiment)$opt[[sample_metadata_column]] == opt$study_label,
+      qcIndex = sampleData(XCMSExperiment)[[opt$sample_metadata_column]] == opt$qc_label,
+      studyIndex = sampleData(XCMSExperiment)[[opt$sample_metadata_column]] == opt$study_label,
       na.rm = opt$na_rm,
-      mad = FALSE
+      mad = opt$mad
     )
 } else if (opt$xcms_experiment == "percent_missing") {
     filter <- PercentMissingFilter(
-      threshold = opt$threshold,
       f = eval(parse(text = opt$f)),
     )
 } else {
     filter <- BlankFlag(
-      threshold = opt$threshold,
-      blankIndex = sampleData(XCMSExperiment)$opt[[sample_metadata_column]] == opt$blank_label,
-      qcIndex = sampleData(XCMSExperiment)$opt[[sample_metadata_column]] == opt$qc_label,
+      blankIndex = sampleData(XCMSExperiment)[[opt$sample_metadata_column]] == opt$blank_label,
+      qcIndex = sampleData(XCMSExperiment)[[opt$sample_metadata_column]] == opt$qc_label,
       na.rm = opt$na_rm,
     )
 }
-
+print(opt)
+print(filter)
+if (!is.null(opt$threshold)) filter@threshold <- opt$threshold
 # Filter features
 XCMSExperiment <- filterFeatures(object = XCMSExperiment, filter = filter)
 
